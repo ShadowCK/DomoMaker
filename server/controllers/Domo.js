4ab3d41d2a23a30f666e63d703dc39f36cc53211
ShadowCK <540712348@qq.com>
2023-11-12 23:27:47 -0500

    Fixed a potential bug

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -16,7 +16,7 @@ const makerPage = async (req, res) => {
 
 const makeDomo = async (req, res) => {
   if (!req.body.name || !req.body.age) {
-    return res.status(400).json('error: Both name and age are required!');
+    return res.status(400).json({ error: 'Both name and age are required!' });
   }
 
   const domoData = {
